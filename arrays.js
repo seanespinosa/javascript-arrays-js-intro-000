01e0dd664b2a5b697fdfc29d320b20ef33e320d4
Sean Espinosa <sean.espinosa1@gmail.com>
2020-04-10 19:56:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,6 +33,7 @@ function removeElementFromBeginningOfArray(array) {
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
