ddb732ce811d74143b2b013392de26ce9616ec78
Sean Espinosa <sean.espinosa1@gmail.com>
2020-04-10 17:49:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array,element) {
   return [element,...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array,elment) {
+function destructivelyAddElementToBeginningOfArray(array,element) {
   array.unshift(element)
   return array
 }
