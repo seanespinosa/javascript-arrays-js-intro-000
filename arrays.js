aac6acfd97da10499795a29382d44b73f5676bf1
Sean Espinosa <sean.espinosa1@gmail.com>
2020-04-10 17:39:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array,element) {
   return chocolateBars
 }
+
+function destructivelyAddElementToBeginningOfArray
